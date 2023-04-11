17064d35c269d9d3c36e4b3dce798c42e19ce20b
gbrath21 <98950608+gbrath21@users.noreply.github.com>
2023-04-11 09:23:43 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/ffs/MainActivity.java
+++ app/src/main/java/com/example/ffs/MainActivity.java
@@ -27,10 +27,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //tablayout er den øverste del af skærmen hvor man kan trykke på de forskellige tabs
         tabLayout = findViewById(R.id.tab_layout);
+        //viewpager er det som er under tabsene som viser de forskellige fragments
         viewPager2 = findViewById(R.id.view_pager);
+        //her bliver der lavet en ny instance af viewpager adapteren som er den java class der
+        //gør at de rigtige fragments bliver vist på de rigtige tabs
         myViewPagerAdapter = new MyViewPagerAdapter(this);
+        //Her vælges adapter classen til at være den adapter som viewpageren skal bruge
         viewPager2.setAdapter(myViewPagerAdapter);
+        //den her funktion 'lytter' efter hvilken tab man står på så den kan vise det rigtige fragment
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -47,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
+        //den her funktion er lidtt ligesom den over men den opfanger hvis man swiper, således
+        //at tabhighlighteren følger med når der swipes mellem siderne/fragmentsene
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
